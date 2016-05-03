de54a86de2f8c80841345385f60e0ed93195e19c
eden barby <eden.barby@uqconnect.edu.au>
2016-05-03 11:25:38 +1000

    more work on the tcp/ip comms. basic echoing working.

--- pizero/basestation/main.cpp
+++ pizero/basestation/main.cpp
@@ -10,18 +10,37 @@
 
 /* includes *******************************************************************/
 
-#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/smart_ptr.hpp>
+#include <boost/thread/thread.hpp>
 
 #include <iostream>
 
 #include <cstdlib>
 #include <cstring>
 
+/* scope declarations *********************************************************/
+
+using boost::asio::ip::tcp;
+
 /* private typedef ************************************************************/
+
+typedef boost::shared_ptr<tcp::socket> socket_ptr;
+
 /* private define *************************************************************/
+
+#define EVER ;;
+
 /* private macro **************************************************************/
 /* private variables **********************************************************/
+
+const int max_length = 1024;
+
 /* private function prototypes ************************************************/
+
+void session(socket_ptr sock);
+void server(boost::asio::io_service& io_service, unsigned short port);
 
 
 /*
@@ -31,6 +50,54 @@
 */
 int main(int argc, char **argv)
 {
+    try {
+        if(argc != 2) {
+            std::cerr << "Usage: blocking_tcp_echo_server <port>\n";
+            return 1;
+        }
+
+        boost::asio::io_service io_service;
+
+        server(io_service, std::atoi(argv[1]));
+    } catch(std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 
     return 0;
+}
+
+void session(socket_ptr sock)
+{
+    try {
+        for(EVER) {
+            char data[max_length];
+
+            boost::system::error_code error;
+
+            size_t length = sock->read_some(boost::asio::buffer(data), error);
+
+            if(error == boost::asio::error::eof) {
+                break; // Connection closed cleanly by peer.
+            } else if(error) {
+                throw boost::system::system_error(error); // Some other error.
+            }
+
+            boost::asio::write(*sock, boost::asio::buffer(data, length));
+        }
+    } catch(std::exception& e) {
+        std::cerr << "Exception in thread: " << e.what() << std::endl;
+    }
+}
+
+void server(boost::asio::io_service& io_service, unsigned short port)
+{
+    tcp::acceptor a(io_service, tcp::endpoint(tcp::v4(), port));
+
+    for(EVER) {
+        socket_ptr sock(new tcp::socket(io_service));
+
+        a.accept(*sock);
+
+        boost::thread t(boost::bind(session, sock));
+    }
 }
--- pizero/rescuedrone/main.cpp
+++ pizero/rescuedrone/main.cpp
@@ -10,13 +10,25 @@
 
 /* includes *******************************************************************/
 
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/smart_ptr.hpp>
+#include <boost/thread/thread.hpp>
+
+#include <iostream>
+
 #include <cstdlib>
 #include <cstring>
-#include <iostream>
-#include <boost/asio.hpp>
+
+/* scope declarations *********************************************************/
+
+using boost::asio::ip::tcp;
 
 /* private typedef ************************************************************/
 /* private define *************************************************************/
+
+#define PACKET_LENGTH_MAX 1024
+
 /* private macro **************************************************************/
 /* private variables **********************************************************/
 /* private function prototypes ************************************************/
@@ -29,5 +41,37 @@
 */
 int main(int argc, char **argv)
 {
+    try {
+        if(argc != 3) {
+            std::cerr << "Usage: " << argv[0] << " <host> <port>" << std::endl;
+            return 1;
+        }
 
+        boost::asio::io_service io_service;
+
+        tcp::resolver resolver(io_service);
+        tcp::resolver::query query(tcp::v4(), argv[1], argv[2]);
+        tcp::resolver::iterator iterator = resolver.resolve(query);
+
+        tcp::socket s(io_service);
+        boost::asio::connect(s, iterator);
+
+        std::cout << "Enter message: ";
+        char request[PACKET_LENGTH_MAX];
+        std::cin.getline(request, PACKET_LENGTH_MAX);
+        size_t request_length = std::strlen(request);
+        boost::asio::write(s, boost::asio::buffer(request, request_length));
+
+        char reply[PACKET_LENGTH_MAX];
+        size_t reply_length = boost::asio::read(s, boost::asio::buffer(reply, request_length));
+
+        std::cout << "Reply is: ";
+        std::cout.write(reply, reply_length);
+        std::cout << std::endl;
+
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
 }
