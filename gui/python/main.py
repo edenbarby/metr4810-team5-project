7bbccad2c67770d44734e7be913a7064ad1944f2
eden barby <eden.barby@uqconnect.edu.au>
2016-05-28 18:24:27 +1000

    fixed speed trackbar, removed other trackbars, some general cleanup

--- gui/python/main.py
+++ gui/python/main.py
@@ -10,36 +10,22 @@ import time
 import threading
 
 
-TRACKBAR_VALUE_SPEED = 100;
-TRACKBAR_VALUE_CLAWOPEN = 0;
-TRACKBAR_VALUE_CLAWCLOSED = 0;
-
-TRACKBAR_MAX_SPEED = 100;
-TRACKBAR_MAX_CLAWOPEN = 100;
-TRACKBAR_MAX_CLAWCLOSED = 100;
-
-
 # Constants.
+CAMERA_ID = 1
 SERIAL_START_BYTE  = 0x12
 SERIAL_STOP_BYTE   = 0x21
 SERIAL_BAUD        = 9600
 SERIAL_BYTESIZE    = serial.EIGHTBITS
 SERIAL_PARITY      = serial.PARITY_NONE
 SERIAL_STOPBITS    = serial.STOPBITS_ONE
-
-# FTDI serial chip ID
 SERIAL_PORT = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AL0227SV-if00-port0"
 
-CAMERA_ID = 0
+# Trackbar Variables
+TRACKBAR_DEFAULT_SPEED = 80;
+TRACKBAR_MAX_SPEED = 100;
 
-
-def trackbar_callback_speed(trackbar_value):
-    TRACKBAR_VALUE_SPEED = trackbar_value
-def trackbar_callback_clawopen(trackbar_value):
-    TRACKBAR_VALUE_CLAWOPEN = trackbar_value
-def trackbar_callback_clawclosed(trackbar_value):
-    TRACKBAR_VALUE_CLAWCLOSED = trackbar_value
-
+def foo(a):
+    pass
 
 # Thread dedicated to capturing video frames. This ensures that the main thread
 # does not have to wait ~50ms for each new image.
@@ -67,7 +53,7 @@ if __name__ == "__main__":
     # Setup default craft state.
     state_motor_left = 100;
     state_motor_right = 100;
-    state_servo_angle = 0;
+    state_servo_angle = 50;
 
     # Instantiate and open serial port.
     serial_obj = serial.Serial(port     = SERIAL_PORT,
@@ -81,9 +67,9 @@ if __name__ == "__main__":
     time_start = time.time()
     time_lasttrans = time.time()
 
+    # Setup the trackbar that controls speed.
     cv2.namedWindow("Display")
-    # cv2.createTrackbar("Motor Speed", "Display", TRACKBAR1_VALUE, TRACKBAR1_VALUE_MAX, trackbar_callback1);
-    # cv2.createTrackbar("Servo Pos", "Display", TRACKBAR2_VALUE, TRACKBAR2_VALUE_MAX, trackbar_callback2);
+    cv2.createTrackbar("Speed", "Display", TRACKBAR_DEFAULT_SPEED, TRACKBAR_MAX_SPEED, foo);
 
     # Setup shutdown event and image frame queue for handling the image capture thread.
     event_shutdown = threading.Event()
@@ -105,41 +91,37 @@ if __name__ == "__main__":
 
         # If it's time to transmit another serial packet, assemble said pack and
         # and transmite it.
-        # if((time.time() - time_lasttrans) > 0.1):
-        #     time_lasttrans = time.time()
+        if((time.time() - time_lasttrans) > 0.1):
+            time_lasttrans = time.time()
 
-        #     packet = struct.pack("BBBBB", SERIAL_START_BYTE, motor_state_left,
-        #                          motor_state_right, servo_state,
-        #                          SERIAL_STOP_BYTE)
+            packet = struct.pack("BBBBB", SERIAL_START_BYTE, state_motor_left,
+                                 state_motor_right, state_servo_angle,
+                                 SERIAL_STOP_BYTE)
 
-        #     # packet = struct.pack("BBBBB", SERIAL_STOP_BYTE, servo_state,
-        #     #                      motor_state_right, motor_state_left,
-        #     #                      SERIAL_START_BYTE)
+            serial_obj.write(packet)
 
-        #     serial_obj.write(packet)
-
-        #     # print "main\tPacket: ", motor_state_left, " ", motor_state_right, " ", servo_state
+            # print "main\tPacket: ", motor_state_left, " ", motor_state_right, " ", servo_state
 
         # Get user input.
         user_input = (cv2.waitKey(1) & 0xFF)
 
         # Handle user input.
-        # if user_input == ord('w'):
-        #     # Forward
-        #     motor_state_left = 100 + TRACKBAR1_VALUE;
-        #     motor_state_right = 100 + TRACKBAR1_VALUE;
-        # if user_input == ord('a'):
-        #     # Turn Left
-        #     motor_state_left = 100 - TRACKBAR1_VALUE;
-        #     motor_state_right = 100 + TRACKBAR1_VALUE;
-        # if user_input == ord('s'):
-        #     # Backwards
-        #     motor_state_left = 100 - TRACKBAR1_VALUE;
-        #     motor_state_right = 100 - TRACKBAR1_VALUE;
-        # if user_input == ord('d'):
-        #     # Turn Right
-        #     motor_state_left = 100 + TRACKBAR1_VALUE;
-        #     motor_state_right = 100 - TRACKBAR1_VALUE;
+        if user_input == ord('w'):
+            # Forward
+            motor_state_left = 100 + cv2.getTrackbarPos("Speed", "Display")
+            motor_state_right = 100 + cv2.getTrackbarPos("Speed", "Display")
+        if user_input == ord('a'):
+            # Turn Left
+            motor_state_left = 100 - cv2.getTrackbarPos("Speed", "Display")
+            motor_state_right = 100 + cv2.getTrackbarPos("Speed", "Display")
+        if user_input == ord('s'):
+            # Backwards
+            motor_state_left = 100 - cv2.getTrackbarPos("Speed", "Display")
+            motor_state_right = 100 - cv2.getTrackbarPos("Speed", "Display")
+        if user_input == ord('d'):
+            # Turn Right
+            motor_state_left = 100 + cv2.getTrackbarPos("Speed", "Display")
+            motor_state_right = 100 - cv2.getTrackbarPos("Speed", "Display")
         if user_input == ord(' '):
             # Stop
             motor_state_left = 100;
